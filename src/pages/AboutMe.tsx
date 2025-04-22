9fbd126aed5994f20826203a36627a040b46d8a7
Christian Taylor <aviationant@gmail.com>
2025-04-22 11:47:42 -0700

    Fixed about me footer

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -4,12 +4,14 @@ const AboutMe = () => {
     return (
         <div className='flex flex-col min-h-screen'>
             <Navbar />
-            <div className='w-full py-[30px] md:py-[50px]'>
-                <div className='max-w-[1240px] mx-auto'>
-                    <div className='grid lg:grid-cols-1 px-4 lg:px-0 text-black'>
-                        <div className='rounded-b-xl bg-[#e8e0c9]'>
-                            <div className='p-3 md:p-5'>
-                                <h2 className=''>About Me</h2>
+            <div className='flex-grow'>
+                <div className='w-full py-[30px] md:py-[50px]'>
+                    <div className='max-w-[1240px] mx-auto'>
+                        <div className='grid lg:grid-cols-1 px-4 lg:px-0 text-black'>
+                            <div className='rounded-b-xl bg-[#e8e0c9]'>
+                                <div className='p-3 md:p-5'>
+                                    <h2 className=''>About Me</h2>
+                                </div>
                             </div>
                         </div>
                     </div>
