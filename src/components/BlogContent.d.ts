9364eeabd14954c98d74893d5f44cd434c65ed13
Christian Taylor <aviationant@gmail.com>
2025-04-28 20:10:47 -0700

    Removed pnpn-lock

--- src/components/BlogContent.d.ts
+++ src/components/BlogContent.d.ts
@@ -1,3 +1,3 @@
 import { BlogsProp } from "../components/Blogs";
-declare const BlogContent: ({ blogs }: BlogsProp) => import("react/jsx-runtime").JSX.Element;
+declare const BlogContent: ({ blogs }: BlogsProp) => import("react/jsx-runtime").JSX.Element | undefined;
 export default BlogContent;
--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -23,8 +23,9 @@ const BlogContent = ({ blogs }) => {
         return (dateText ? dateText[1] : "");
     }
     var date = dateToPST(blog.publishedAt);
-    console.log(blog.blogContent);
     const content = blog.blogContent;
+    if (!blogs)
+        return;
     return (_jsx("div", { className: 'w-full py-[30px] md:py-[50px]', children: _jsx("div", { className: 'max-w-[1240px] mx-auto', children: _jsxs("div", { className: 'grid lg:grid-cols-1 px-4 lg:px-0 text-black', children: [_jsx("img", { className: 'h-56 w-full object-cover rounded-t-xl', src: blog.coverImg[0].url }), _jsx("div", { className: 'rounded-b-xl bg-[#e8e0c9]', children: _jsxs("div", { className: 'p-3 md:p-5', children: [_jsx("h3", { className: 'font-bold text-4xl md:my-1 text-black', children: blog.blogTitle }), _jsxs("h2", { className: 'text-xl', children: ["Written by ", blog.authorName] }), _jsx("p", { className: 'text-black text-l italic', children: date }), _jsx("hr", { className: 'mt-4 mb-4 mr-[calc(33%)] h-px' }), _jsx(BlocksRenderer, { content: content })] }) })] }) }) }));
 };
 export default BlogContent;
